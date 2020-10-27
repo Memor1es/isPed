66ad355dd6dcca25348c6571f18bb7e7a7088181
Yagiz ALP <yagizwasmeow@gmail.com>
2020-10-27 19:51:06 +0300

    Update isPed_sv.lua

--- isPed_sv.lua
+++ isPed_sv.lua
@@ -2,17 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback("isPed:getIdentifier", function(source, cb)
-    local src = source
-    local ply = ESX.GetPlayerFromId(src)
-
-    if ply then
-        local cid = ply.getIdentifier()
-        
-        cb(cid)
-    end
-end)
-
 ESX.RegisterServerCallback("isPed:getBank", function(source, cb)
     local src = source
     local ply = ESX.GetPlayerFromId(src)
